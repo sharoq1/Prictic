861a14bfa57a2ae2647fae07a61e5a68cfa2d3fa
edris <edrissharoq@gmail.com>
2022-06-22 10:08:23 -0700

    Practice

---
+++ src/GitPractic/Practic.java
@@ -0,0 +1,12 @@
+package GitPractic;
+
+public class Practic {
+
+	public static void main(String[] args) {
+		double a =25.5;
+		double b = 583.5;
+		double c = a+b;
+		System.out.println(c);
+	}
+
+}
